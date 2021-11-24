0957088ae3e78786b7d33c7585f34a6b38b29fe8
Nino Paul Cervantes <oninpaul15@gmail.com>
2021-11-24 10:32:55 +0800

    reducers

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import store from './src/services/store';
 import Navigation from './src/navigations';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <Navigation />
-      <StatusBar />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar />
+      </SafeAreaProvider>
+    </Provider>
+    
   );
 }
