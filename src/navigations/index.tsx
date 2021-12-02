a07f7536c61c6786ee95591a9767b58856b6d311
Nino Paul Cervantes <oninpaul15@gmail.com>
2021-12-02 19:21:34 +0800

    update

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import App from '@screens/app';
 import Registration from '@screens/registration';
 import Login from '@screens/login';
 import Home from '@screens/home';
 
 
 type RootStackParamList = {
+  App: undefined;
   Login: undefined;
   Registration: undefined;
   Home: undefined;
@@ -17,11 +19,13 @@ const RootNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
+        
         screenOptions={{
           gestureEnabled: false,
           headerShown: false
         }}
       >
+        <Stack.Screen name="App" component={App} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Registration" component={Registration} />
         <Stack.Screen name="Home" component={Home} />
