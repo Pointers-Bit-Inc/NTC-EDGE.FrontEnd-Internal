0cd45f7acda1f363b0e23bdff63fae539282ea45
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-01-24 15:45:25 +0800

    update fix bug

--- src/components/pages/activities/application/paymentModal/index.tsx
+++ src/components/pages/activities/application/paymentModal/index.tsx
@@ -3,7 +3,7 @@ import BackgroundPayment from "@assets/svg/backgroundpayment";
 import {Dimensions, Modal, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import moment from "moment";
 
-
+const {width, height} = Dimensions.get('window');
 const PaymentModal = (props: any) => {
 
     return <Modal
@@ -87,5 +87,149 @@ const PaymentModal = (props: any) => {
     </Modal>
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    group7: {
 
+        height: 100
+    },
+    rect2: {
+        width: width,
+        height: 100,
+        backgroundColor: "rgba(0,65,172,1)"
+    },
+    close: {
+        color: "rgba(239,231,231,1)",
+        fontSize: 18,
+    },
+    group8: {
+        width: "100%",
+        height: 473,
+        marginTop: 70,
+        alignSelf: "center"
+    },
+    group5: {
+        alignItems: "center"
+    },
+    paymentReceivedFor: {
+        color: "#121212",
+        textAlign: 'center'
+    },
+    ntcEdge: {
+        color: "#121212",
+        marginTop: 4,
+        textAlign: 'center'
+    },
+    theAmoutOf: {
+        color: "#121212",
+        marginTop: 24,
+        textAlign: 'center'
+    },
+    php5000: {
+        color: "#121212",
+
+    },
+    loremIpsum: {
+        color: "#121212",
+        marginTop: 25
+    },
+    group2: {
+        width: '100%',
+        marginTop: 30,
+        alignItems: 'center'
+    },
+    rect: {
+        width: width * 0.65,
+        padding: 15,
+        paddingVertical: 25,
+        borderWidth: 1,
+        borderColor: "rgba(151,151,151,1)",
+        borderStyle: "dashed",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    group: {},
+    refNo12345678910: {
+        color: "#121212",
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    text: {
+        color: "#121212",
+        marginTop: 11,
+        textAlign: 'center',
+    },
+    group6: {
+
+        height: 187,
+        marginTop: 23
+    },
+    details: {
+        fontWeight: "bold",
+        color: "#121212",
+        alignSelf: "center"
+    },
+    group3: {
+        height: 148,
+        alignItems: "flex-start",
+        justifyContent: "space-around"
+    },
+    email: {
+        color: "#121212"
+    },
+    fee: {
+        color: "#121212"
+    },
+    accountNumber: {
+        color: "#121212"
+    },
+    account2: {
+        color: "#121212"
+    },
+    amountPaid: {
+        color: "#121212"
+    },
+    group3Filler: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    group4: {
+
+        height: 148,
+        justifyContent: "space-around"
+    },
+    emailInput: {
+        color: "#121212",
+        alignSelf: "stretch",
+        textAlign: 'right'
+    },
+    php000: {
+        color: "#121212",
+        alignSelf: "stretch",
+        textAlign: 'right'
+    },
+    loremIpsum3: {
+        color: "#121212",
+        alignSelf: "stretch",
+        textAlign: 'right'
+    },
+    jmGrills: {
+        color: "#121212",
+        alignSelf: "stretch",
+        textAlign: 'right'
+    },
+    php50003: {
+        color: "#121212",
+        alignSelf: "stretch",
+        textAlign: 'right'
+    },
+    group3Row: {
+        height: 148,
+        flexDirection: "row",
+        marginTop: 22,
+        marginRight: 1
+    }
+});
 export default PaymentModal
