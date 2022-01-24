8cd77f8ed0fad6f8f5336738d30c90bdfc2906a1
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-01-24 17:49:48 +0800

    update fix bug

--- src/components/pages/activities/modal.tsx
+++ src/components/pages/activities/modal.tsx
@@ -74,8 +74,6 @@ function ActivityModal(props: any) {
                 remarks: remarks ? remarks : undefined,
             };
         }
-
-              console.log(url, params)
         if (applicationId) {
             await api.patch(url, params)
                 .then(res => {
@@ -83,6 +81,7 @@ function ActivityModal(props: any) {
                     setCurrentLoading('');
                     if (res.status === 200) {
                         if (res.data) {
+                            console.log(200)
                             dispatch(updateApplicationStatus({
                                 application: res.data,
                                 status: status,
@@ -92,6 +91,8 @@ function ActivityModal(props: any) {
 
                             setStatus(cashier ? PaymentStatusText(status) : StatusText(status))
                             setChange(true)
+                            // props.onDismissed(true, applicationId)
+
                             return callback(null, applicationId);
                         }
                     }
@@ -115,22 +116,21 @@ function ActivityModal(props: any) {
         setShowAlert(true)
 
     }
-         const [id, setId] = useState()
+
     useEffect(() => {
         setChange(false)
         setStatus("")
-
+        setAssignId(props.details.assignedPersonnel)
     }, [])
     const statusMemo = useMemo(() => {
         setStatus(status)
-        setAssignId(props.details.assignedPersonnel)
-        setId(props.details.assignedPersonnel)
+
         return status ? (cashier ? PaymentStatusText(status) : StatusText(status)) : (cashier ? PaymentStatusText(props.details.paymentStatus) : StatusText(props.details.status))
-    }, [assignId, id, status, props.details.paymentStatus, props.details.status])
+    }, [assignId, status, props.details.paymentStatus, props.details.status])
     const approveButton = cashier ? statusMemo === APPROVED || statusMemo === VERIFIED : (statusMemo === APPROVED || statusMemo === VERIFIED)
     const declineButton = cashier ? (statusMemo === UNVERIFIED || statusMemo === DECLINED) : statusMemo === DECLINED
     const allButton =  (statusMemo == FORVERIFICATION || statusMemo == PENDING || statusMemo == FOREVALUATION) && [CASHIER, EVALUATOR].indexOf(user?.role?.key) != -1 && assignId != user?._id ? true :  (declineButton || approveButton || grayedOut)
-             console.log(assignId, id)
+    console.log( assignId != user?.id, props.details.paymentStatus, props.details.status, [CASHIER, EVALUATOR].indexOf(user?.role?.key) != -1 , statusMemo == FORVERIFICATION, statusMemo, props.details.assignedPersonnel, "[assign id:", assignId , ']', user?._id, [EVALUATOR].indexOf(user?.role?.key) != -1, assignId == user?._id, assignId == null)
 
     return (
         <Modal
