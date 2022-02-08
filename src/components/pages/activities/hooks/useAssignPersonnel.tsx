14f48a32485dc4819537e5259b8df8c5c7c3c7ca
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-02-08 19:59:34 +0800

    addition update

--- src/components/pages/activities/hooks/useAssignPersonnel.tsx
+++ src/components/pages/activities/hooks/useAssignPersonnel.tsx
@@ -5,16 +5,20 @@ import {BASE_URL} from "../../../../services/config";
 
 export function useAssignPersonnel(assignedPersonnel, config) {
     const [personnel, setPersonnel] = useState<UserApplication>()
+    const [prevPersonnel, setPrevPersonnel] = useState<UserApplication>()
     const [loading, setLoading] = useState<boolean>()
     const fetchData = async () => {
         setLoading(true)
+
         await axios
             .get(BASE_URL + `/user/profile/${assignedPersonnel}`, config)
             .then((res) => {
                 setLoading(false)
                 setPersonnel(res.data);
+                setPrevPersonnel(res.data);
             })
             .catch((err) => {
+                setPersonnel(prevPersonnel);
                 setLoading(false)
                 console.log(err);
             });
