aa4498254c69561fde1b141ad51d8aa01b579f76
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-02-14 06:59:12 +0800

    update

--- src/components/pages/activities/activityItem.tsx
+++ src/components/pages/activities/activityItem.tsx
@@ -205,7 +205,7 @@ export function ActivityItem(props:any) {
                                         height: 0,
                                         width: 0
                                     },
-                                    elevation: 10,
+                                    elevation: 1,
                                     shadowOpacity: 0.1,
                                     shadowRadius: 2,
                                     padding: 10,
