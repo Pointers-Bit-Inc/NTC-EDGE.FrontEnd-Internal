503bde992d6615efa23f90d0d9db94601c7b7005
Bernadette Villajin <bernadette.villajin@ustp.edu.ph>
2022-02-16 17:00:02 +0800

    inputfield

--- src/components/molecules/form-fields/input-field.tsx
+++ src/components/molecules/form-fields/input-field.tsx
@@ -1,209 +1,99 @@
-import React, {
-    useRef,
-    useState,
-    forwardRef,
-    useImperativeHandle,
-    ForwardRefRenderFunction,
-} from 'react';
-import {View , StyleSheet , TouchableOpacity} from 'react-native';
-import { ExclamationIcon } from '@atoms/icon';
+import React, { FC, useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import Text from '@atoms/text';
-import TextInput from '@components/atoms/input';
-import {defaultColor , primaryColor} from '@styles/color';
- import inputStyles from "@styles/input-style"
- import {input} from "@styles/color"
-import CloseIcon from "@assets/svg/close";
-const styles = StyleSheet.create({
-    container: {
-        paddingVertical: 10
-    },
-    label: {
-        position: 'absolute',
-        top: 2,
-        left: 15,
-        paddingHorizontal: 5,
-        backgroundColor: 'white',
-        zIndex: 99,
-        flexDirection: 'row'
-    },
-    inputContainer: {
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 10,
-        overflow: 'hidden',
-        width: '100%'
-    },
-    description: {
-        paddingTop: 2,
-    },
-    info: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    }
-});
+import TextInput from '@atoms/input';
+import { CloseIcon } from '@atoms/icon';
+import styles from '@styles/input-style';
+import { input } from '@styles/color';
 
 interface Props {
-    label?: string;
-    placeholder?: string;
-    secureTextEntry?: boolean;
-    required?: boolean;
-    hasValidation?: boolean;
-    containerStyle?: any;
-    inputStyle?: any;
-    labelStyle?: any;
-    outlineStyle?: any;
-    description?: string;
-    error?: string;
-    errorColor?: string;
-    activeColor?: string;
-    requiredColor?: string;
-    onBlur?: () => void;
-    onFocus?: () => void;
-    [x: string]: any;
+  label?: string;
+  placeholder?: string;
+  secureTextEntry?: boolean;
+  required?: boolean;
+  hasValidation?: boolean;
+  description?: string;
+  error?: string;
+  [x: string]: any;
 }
 
-export type TextInputRef =  {
-    blur: any,
-    focus: any,
-}
+const InputField: FC<Props> = ({
+  label = '',
+  placeholder = '',
+  secureTextEntry = false,
+  required = false,
+  hasValidation = false,
+  description,
+  error,
+  ...otherProps
+}) => {
+  const { text, background } = input;
 
-const InputField: ForwardRefRenderFunction<TextInputRef, Props> = ({
-      clearable=true,
-      label = '',
-      placeholder = '',
-      secureTextEntry = false,
-      required = false,
-      hasValidation = false,
-      containerStyle,
-      inputStyle,
-      labelStyle,
-      outlineStyle,
-      description,
-      error,
-      errorColor,
-      activeColor,
-      requiredColor,
-      disabledColor,
-      onBlur = () => {},
-      onFocus = () => {},
-      ...otherProps
-  }, ref) => {
-    const inputRef:any = useRef(null);
-    const [isFocused, setIsFocused] = useState(false);
-    const editable = otherProps?.editable === false || otherProps?.editable === true ? otherProps?.editable : true;
-    const onFocusFN = () => {
-        setIsFocused(true)
-        onBlur(false);
-        onFocus(true);
-    };
-    const onBlurFN = () => {
-        setIsFocused(false)
-        onFocus(false);
-        onBlur(true);
-    };
-    useImperativeHandle(ref, () => ({
-        blur: () => inputRef.current.blur(),
-        focus: () => inputRef.current.focus(),
-    }));
-    return (
-        <View style={inputStyles.mainContainer}>
-            
-            <View style={[
+  const [focused, setFocused] = useState(false);
+  const onFocus = () => setFocused(true);
+  const onBlur = () => setFocused(false);
 
-                inputStyles.container,
-
-                inputStyles.rowContainer,
-
-                !!error && {
-                    backgroundColor: input.background?.error,
-                    borderColor: input.text?.errorColor,
-                },
-                !error && isFocused && {
-                    backgroundColor: input.background?.default,
-                    borderColor: primaryColor,
-                },
-                containerStyle,
-            ]}>
-                <View style={{ flex: 0.95 }}>
-                    {!!otherProps.value && !!label && (
-                        <View style={inputStyles.labelContainer}>
-                            <Text
-                                style={[
-                                    inputStyles.labelText,
-                                    (isFocused || !!otherProps.value || !!error) && {
-                                        color: isFocused ? activeColor : "#808196"
-                                    },
-
-                                    !editable && {color: disabledColor}
-                                ]}
-                            >
-                                {label}
-                            </Text>
-                            {required && (
-                                <Text style={[inputStyles.requiredText,
-                                    !!requiredColor && {
-                                        color:  isFocused ? requiredColor : "#808196"
-                                    },
-                                    !editable && {color: disabledColor}]}>
-                                    {'*'}
-                                </Text>
-                            )}
-                        </View>
-                    )}
-                    <View
-                        style={[
-
-                            outlineStyle,
-                            isFocused && { borderColor: activeColor },
-                            !!error && { borderColor: errorColor },
-                            !editable && {borderColor: disabledColor}
-                        ]}
-                    >
-                        <TextInput
-                        ref={inputRef}
-                        style={[inputStyle,inputStyles.inputText, !editable && {color: disabledColor}]}
-                        placeholder={placeholder || label}
-                        placeholderTextColor={!editable ? disabledColor : (!!error ? input.text?.errorColor : defaultColor)}
-                        secureTextEntry={secureTextEntry}
-                        onFocus={onFocusFN}
-                        onBlur={onBlurFN}
-                        {...otherProps}
-                    />
-                    </View>
-                </View>
-                {
-                    (clearable && isFocused && !!editable && !!otherProps.value) &&
-                    <TouchableOpacity onPress={() => otherProps?.onChangeText ? otherProps?.onChangeText('') : {}}>
-                        <CloseIcon size={20} color={error ? input.text.errorColor : input.text.mainColor}/>
-                    </TouchableOpacity>
-                }
+  return (
+    <View style={styles.mainContainer}>
+      <View style={[
+        styles.container,
+        styles.rowContainer,
+        !!error && {
+          backgroundColor: background?.error,
+          borderColor: text?.errorColor,
+        }
+      ]}>
+        <View style={{ flex: 0.95 }}>
+          {!!otherProps.value && !!label && (
+            <View style={styles.labelContainer}>
+              <Text
+                style={[
+                  styles.labelText,
+                  !!error && {color: text?.errorColor}
+                ]}
+              >
+                {label}
+              </Text>
+              {required && (
+                <Text style={styles.requiredText}>
+                  {'*'}
+                </Text>
+              )}
             </View>
-
-            {
-                hasValidation && (!!error || !!description) && (
-                    <View>
-                        <Text
-                            style={[
-                                inputStyles?.validationText,
-                                !!error && { color: input.text?.errorColor },
-                            ]}
-                        >
-                            {error || description}
-                        </Text>
-                    </View>
-                )
-            }
+          )}
+          <TextInput
+            style={styles.inputText}
+            placeholder={placeholder || label}
+            placeholderTextColor={!!error ? text?.errorColor : text?.defaultColor}
+            secureTextEntry={secureTextEntry}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            {...otherProps}
+          />
         </View>
-
-
-        
-
-
-
-
-
-
-    );
+        {
+          !!otherProps.value && focused &&
+          <TouchableOpacity onPress={() => otherProps?.onChangeText ? otherProps?.onChangeText('') : {}}>
+            <CloseIcon size={15} color={error ? text.errorColor : text.mainColor}/>
+          </TouchableOpacity>
+        }
+      </View>
+      {
+        hasValidation && (!!error || !!description) && (
+          <View>
+            <Text
+              style={[
+                styles?.validationText,
+                !!error && { color: text?.errorColor },
+              ]}
+            >
+              {error || description}
+            </Text>
+          </View>
+        )
+      }
+    </View>
+  );
 };
 
-export default forwardRef(InputField);
+export default InputField;
