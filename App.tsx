933f6c8d98980a6d1022e3cf4ac112183655f46d
Nino Paul Cervantes <oninpaul15@gmail.com>
2022-02-28 16:27:57 +0800

    temporary web solution

--- App.tsx
+++ App.tsx
@@ -6,11 +6,6 @@ import { store, persistor } from 'src/services/store';
 import Navigation from 'src/navigations';
 import TopModal from "@pages/activities/topmodal";
 
-
-if(__DEV__) {
-    import("./ReactotronConfig")
-}
-
 export default function App() {
     return (
     <Provider store={store}>
