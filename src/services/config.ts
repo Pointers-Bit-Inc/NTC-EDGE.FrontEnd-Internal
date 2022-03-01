a25d6006ed782a75b16ca3f5ba93077be5d4b429
Nino Paul Cervantes <oninpaul15@gmail.com>
2022-03-01 13:37:04 +0800

    dev env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://prod-edge-application-processing-appsvc.azurewebsites.net';
+const BASE_URL = 'https://test-edge-application-processing-appsvc.azurewebsites.net';
 const BASE_URL_NODE = 'https://ntc.astrotechenergy.com';
 const firebaseConfig = {
   apiKey: "AIzaSyASZc6WOG4-kKMf7I98p_5aRjbXHzWsmyU",
