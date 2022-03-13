769e2a1b87a2fb5c2d035b68ad49c476fa98d999
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-03-13 19:19:03 +0800

    update forgot password

--- src/screens/forgot-password/otp.tsx
+++ src/screens/forgot-password/otp.tsx
@@ -1,65 +1,20 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { Dimensions, ActivityIndicator, View, SafeAreaView, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
-import Text from '@components/atoms/text';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+import Text from '@atoms/text';
 import * as Progress from 'react-native-progress';
-import InputField from '@atoms/input';
 import { OTPField } from '@molecules/form-fields';
-import Button from '@components/atoms/button';
-import { ArrowLeftIcon } from '@components/atoms/icon';
+import Button from '@atoms/button';
 import { text, button } from 'src/styles/color';
-import InputStyles from 'src/styles/input-style';
-
-const { height } = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  header: {
-    marginTop: 10,
-    padding: 20,
-    paddingBottom: 0,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  content: {
-    padding: 20,
-    flex: 1,
-  },
-  description: {
-    paddingVertical: 25,
-  },
-  button: {
-    backgroundColor: button.primary,
-    borderRadius: 5,
-    marginHorizontal: 20,
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'center',
-  },
-  input: {
-    letterSpacing: 8,
-    fontSize: 36,
-    fontWeight: 'bold',
-    flex: 1,
-    paddingHorizontal: 0,
-  },
-  outlineStyle: {
-    paddingHorizontal: 0,
-    borderWidth: 0,
-  },
-  labelStyle: {
-    fontSize: 14,
-    color: text.default,
-  },
-  keyboardAvoiding: {
-    position: 'absolute',
-    bottom: 30,
-    width: '100%',
-  },
-})
+import useKeyboard from 'src/hooks/useKeyboard';
+import Ellipsis from '@atoms/ellipsis';
+import styles from './styles';
+import NavBar from "@molecules/navbar";
+import Left from "@atoms/icon/left";
 
 const timerLimit = 90;
 const errorResponse = 'Invalid verification code. Please try again';
@@ -68,26 +23,25 @@ const code = '1234';
 const OneTimePin = ({ navigation, route }:any) => {
   const {
     account,
-    accountType
+    accountType,
+    token,
   } = route.params;
+  const isKeyboardVisible = useKeyboard();
   const [timer, setTimer] = useState(timerLimit);
   const [started, setStarted] = useState(false);
   const [ended, setEnded] = useState(false);
   const [loading, setLoading] = useState(false);
   const [otp, setOtp] = useState('');
   const [error, setError] = useState('');
-  const onSubmit = useCallback(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      if (otp === code) {
-        setEnded(true);
-        navigation.navigate('ForgotPasswordReset')
-      } else {
-        setError(errorResponse);
-      }
-    }, 3000);
-  }, [otp]);
+  const onSubmit = () => {
+    if (otp === code) {
+      setEnded(true);
+      navigation.navigate('ForgotPasswordReset', {token});
+    } else {
+      setError(errorResponse);
+    }
+  };
+
   const onResend = useCallback(() => {
     setLoading(true);
     setTimeout(() => {
@@ -104,7 +58,7 @@ const OneTimePin = ({ navigation, route }:any) => {
     setOtp('');
   }
 
-  const format = (time:number) => {   
+  const format = (time:number) => {
     var mins = ~~((time % 3600) / 60);
     var secs = ~~time % 60;
     var format = "";
@@ -133,95 +87,6 @@ const OneTimePin = ({ navigation, route }:any) => {
       setEnded(true);
     }
   }, [timer]);
-  
-  const renderButton = () => {
-    if (ended || error) {
-      return (
-        <Button
-          disabled={loading}
-          style={[
-            styles.button,
-            {
-              backgroundColor: button.success
-            },
-            loading && {
-              backgroundColor: '#3BC759'
-            }
-          ]}
-          onPress={onResend}
-        >
-          {
-            loading ? (
-              <>
-                <ActivityIndicator
-                  color={'white'}
-                  size={'small'}
-                />
-                <Text
-                  style={{ marginLeft: 10 }}
-                  color="white"
-                  size={16}
-                  weight={'500'}
-                >
-                  Sending code...
-                </Text>
-              </>
-            ) : (
-              <Text
-                color="white"
-                size={16}
-                weight={'500'}
-              >
-                Resend code
-              </Text>
-            )
-          }
-        </Button>
-      )
-    }
-    return (
-      <Button
-        disabled={otp.length < 4 || loading}
-        style={[
-          styles.button,
-          otp.length < 4 && {
-            backgroundColor: button.default
-          },
-          loading && {
-            backgroundColor: '#60A5FA'
-          }
-        ]}
-        onPress={onSubmit}
-      >
-        {
-          loading ? (
-            <>
-              <ActivityIndicator
-                color={'white'}
-                size={'small'}
-              />
-              <Text
-                style={{ marginLeft: 10 }}
-                color="white"
-                size={16}
-                weight={'500'}
-              >
-                Confirming...
-              </Text>
-            </>
-          ) : (
-            <Text
-              color="white"
-              size={16}
-              weight={'500'}
-            >
-              Confirm
-            </Text>
-          )
-        }
-      </Button>
-    )
-  }
 
   const renderDetail = () => {
     if (accountType === 'phone') {
@@ -240,101 +105,144 @@ const OneTimePin = ({ navigation, route }:any) => {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <ArrowLeftIcon
-            size={26}
-            color={text.default}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Login')}
+      <View style={styles.container}>
+        <NavBar
+            title={'Forgot Password'}
+            rightIcon={<Text color='#fff'>Close</Text>}
+            onRight={() => navigation.navigate('Login')}
+            leftIcon={<Left color='#fff' size={17}/>}
+            onLeft={() => navigation.pop()}
+        />
+
+        <KeyboardAvoidingView
+            behavior={Platform.select({ ios: 'padding', android: undefined })}
+            keyboardVerticalOffset={Platform.select({ ios: 75, android: undefined })}
+            style={styles.container}
         >
-          <Text
-            size={18}
-            color={text.default}
+          <ScrollView
+              style={styles.scrollview}
+              showsVerticalScrollIndicator={false}
           >
-            Close
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.content}>
-        <Text
-          size={22}
-          color={text.default}
-          weight={'600'}
-        >
-          OTP code sent!
-        </Text>
-        <View style={styles.description}>
-          <Text
-            size={16}
-            color={text.default}
-          >
-            {accountType === 'phone' ?
-              `Enter the OTP code we sent via SMS to your registered phone number ${renderDetail()}.` :
-              `Enter the OTP code we sent via EMAIL to your registered email address ${renderDetail()}.`
-            }
-          </Text>
-        </View>
-        <OTPField
-          style={[
-            InputStyles.text,
-            styles.input,
-            !!error && {
-              color: text.error
-            }
-          ]}
-          maxLength={4}
-          placeholder="••••"
-          label={'OTP'}
-          labelStyle={styles.labelStyle}
-          required={true}
-          hasValidation={true}
-          outlineStyle={[InputStyles.outlineStyle, styles.outlineStyle]}
-          errorColor={text.error}
-          requiredColor={text.error}
-          error={error}
-          value={otp}
-          keyboardType={'number-pad'}
-          onChangeText={onChangeText}
-        >
-          {
-            !ended && (
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Progress.Pie
-                  style={{
-                    transform: [
-                        {scaleX: -1},
-                    ]
-                  }}
-                  color={text.success}
-                  borderWidth={0}
-                  progress={timer/timerLimit}
-                  size={16}
-                />
-                <Text
-                  style={{ marginLeft: 5 }}
+            <Text
+                size={22}
+                color={text.default}
+            >
+              OTP code sent!
+            </Text>
+            <View style={styles.description}>
+              <Text
                   size={16}
                   color={text.default}
-                >
-                  {format(timer)}
-                </Text>
-              </View>
-            )
-          }
-        </OTPField>
+              >
+                {accountType === 'phone' ?
+                 `Enter the OTP code sent via SMS to your registered phone number ${renderDetail()}.` :
+                 `Enter the OTP code sent via EMAIL to your registered email address ${renderDetail()}.`
+                }
+              </Text>
+            </View>
+            <OTPField
+                style={[
+                  styles.input,
+                  !!error && {
+                    color: text.error
+                  }
+                ]}
+                maxLength={4}
+                placeholder="••••"
+                label={'OTP'}
+                labelStyle={styles.labelStyle}
+                required={true}
+                hasValidation={true}
+                outlineStyle={styles.outlineStyle}
+                errorColor={text.error}
+                requiredColor={text.error}
+                error={error}
+                value={otp}
+                keyboardType={'number-pad'}
+                onChangeText={onChangeText}
+            >
+              {
+                !ended && (
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Progress.Pie
+                          style={{
+                            transform: [
+                              {scaleX: -1},
+                            ]
+                          }}
+                          color={text.success}
+                          borderWidth={0}
+                          progress={timer/timerLimit}
+                          size={16}
+                      />
+                      <Text
+                          style={{ marginLeft: 5 }}
+                          size={16}
+                          color={text.default}
+                      >
+                        {format(timer)}
+                      </Text>
+                    </View>
+                )
+              }
+            </OTPField>
+          </ScrollView>
+        </KeyboardAvoidingView>
+
         <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'position' : 'height'}
-          keyboardVerticalOffset={height * 0.12}
-          style={styles.keyboardAvoiding}
+            behavior={Platform.select({ ios: 'position', android: undefined })}
+            style={styles.keyboardAvoiding}
         >
-          {renderButton()}
+          <View style={isKeyboardVisible && styles.shadow}>
+            <Button
+                disabled={loading}
+                style={[
+                  styles.button,
+                  {
+                    backgroundColor: (ended || error)
+                                     ? loading
+                                       ? '#3BC759'
+                                       : button.success
+                                     : loading
+                                       ? '#60A5FA'
+                                       : otp.length < 4
+                                         ? button.default
+                                         : button.primary
+                  },
+                ]}
+                onPress={(ended || error) ? onResend : onSubmit}
+            >
+              {
+                loading
+                ? <Ellipsis color='#fff' size={10} />
+                : <Text
+                    color={(ended || error)
+                           ? '#fff'
+                           : loading
+                             ? '#fff'
+                             : otp.length < 4
+                               ? text.disabled
+                               : '#fff'
+                    }
+                    size={16}
+                    style={styles.boldText}
+                >
+                  {
+                    (ended || error)
+                    ? loading
+                      ? 'Resending code...'
+                      : 'Resend code'
+                    : loading
+                      ? 'Confirming...'
+                      : 'Confirm'
+                  }
+                </Text>
+              }
+            </Button>
+          </View>
         </KeyboardAvoidingView>
       </View>
-    </SafeAreaView>
   )
 }
 
-export default OneTimePin
+export default OneTimePin;
