7be67ec79643e20fe3d484bd55cc316aa85b9170
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-03-28 13:29:54 +0800

    update

--- src/components/pages/activities/script.tsx
+++ src/components/pages/activities/script.tsx
@@ -272,7 +272,7 @@ export function getStatusText(props: any , personnel: UserApplication | undefine
         props?.activity?.approvalHistory?.[0]?.status !== FORAPPROVAL && props?.activity?.approvalHistory?.status !== FORAPPROVAL || (
             props?.activity?.assignedPersonnel?._id !== props.currentUser?._id)) ? StatusText(APPROVED) : getRole(props.user , [ACCOUNTANT]) && !!props.paymentMethod && (
         !!props?.paymentHistory?.[0]?.status && !!props?.paymentHistory?.status) ? StatusText(props.paymentHistory?.[0]?.status || props.paymentHistory?.status) : getRole(props.user , [ACCOUNTANT]) && (
-        props.approvalHistory?.[0]?.status == FOREVALUATION && props.approvalHistory?.status == FOREVALUATION) && props.approvalHistory[1].status == FORAPPROVAL ? DECLINED : getRole(props.user , [CASHIER]) && (
+        props.approvalHistory?.[0]?.status == FOREVALUATION && props.approvalHistory?.status == FOREVALUATION) && props.approvalHistory[1].status == FORAPPROVAL ? DECLINED : getRole(props.user , [CASHIER]) && !(props?.activity?.assignedPersonnel) || (
         props?.paymentHistory?.[0]?.status == PENDING && props?.paymentHistory?.status == PENDING) ? FORVERIFICATION : getStatus(props , personnel);
 }
 
