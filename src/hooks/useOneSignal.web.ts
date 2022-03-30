aa03309fe36dd9cb8ed5f50bc64ee4965d5277f8
Nino Paul Cervantes <oninpaul15@gmail.com>
2022-03-30 19:08:58 +0800

    update

---
+++ src/hooks/useOneSignal.web.ts
@@ -0,0 +1,17 @@
+import Constants from "expo-constants";
+import IUser from 'src/interfaces/IUser';
+
+const useOneSignal = (user:IUser) => {
+  const initialize = () => {
+    
+  }
+
+  const destroy = () => {}
+
+  return {
+    initialize,
+    destroy,
+  }
+}
+
+export default useOneSignal
