606d5a8db503c4b76bc1dfaad6d9604974be099f
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-03-31 14:51:39 +0800

    update

--- src/components/pages/activities/index.tsx
+++ src/components/pages/activities/index.tsx
@@ -199,9 +199,7 @@ export default function ActivitiesPage(props: any) {
 
             <View style={ { backgroundColor : "#F8F8F8" , flex : 1 , flexDirection : "row" } }>
                 <View onLayout={ onActivityLayoutComponent } style={ [styles.container , styles.shadow , {
-
-                    flex : (
-                               isMobile || dimensions?.width < 768) ? 1 : 0.4 ,
+    
                     flexBasis : (
                                     isMobile || dimensions?.width < 768) ? "100%" : 466 ,
                     flexGrow : 0 ,
