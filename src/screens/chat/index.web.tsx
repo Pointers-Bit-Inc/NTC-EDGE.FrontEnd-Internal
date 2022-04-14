80f1db978f8280332778f06eaec880529ca2c7fe
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-04-14 18:19:27 +0800

    update

--- src/screens/chat/index.web.tsx
+++ src/screens/chat/index.web.tsx
@@ -48,8 +48,8 @@ import {useComponentLayout} from "../../hooks/useComponentLayout";
 import {setChatLayout} from "../../reducers/layout/actions";
 import {Hoverable} from "react-native-web-hooks";
 import GroupImage from "@molecules/image/group";
-//import FileList from "@screens/chat/file-list";
-import FileList from '@components/organisms/chat/files';
+import FileList from "@screens/chat/file-list";
+//import FileList from '@components/organisms/chat/files';
 import NoConversationIcon from "@assets/svg/noConversations";
 import {isMobile} from "@pages/activities/isMobile";
 import {ViewPaged} from 'react-scroll-paged-view'
@@ -787,7 +787,7 @@ const ChatList=({navigation}:any)=>{
     };
     useEffect(()=>{
         
-        if(selectedFile){
+        if(lodash.size(selectedFile)){
             let uri = selectedFile?.uri;
             let split = uri?.split('/');
             let name = split?.[split?.length - 1];
@@ -797,21 +797,14 @@ const ChatList=({navigation}:any)=>{
                 mimeResult=mime[1];
             }
             let mimeType=mimeResult?.split("/")?.[1];
-
-
-
-
             fetch(uri)
             .then(res=>{
-
                 return res?.blob()
             })
             .then(blob=>{
-
                 const fd=new FormData();
                 const _file= new File([blob],(
                     name+"."+mimeType));
-
                 fd.append('file',_file,(
                     name+"."+mimeType));
                 const API_URL=`${BASE_URL}/messages/${_id}/upload-file`;
@@ -824,17 +817,18 @@ const ChatList=({navigation}:any)=>{
                 .then(res=>{
 
                     return res?.json()
+                }).then(res =>{
+                    _sendFile(
+                        channelId,
+                        selectedFile,
+                        name||"",
+                        participants
+                    )
                 })
 
             })
-            if(lodash.size(selectedFile)){
-                _sendFile(
-                    channelId,
-                    selectedFile,
-                    name||"",
-                    participants
-                )
-            }
+
+
 
         }
 
@@ -1203,7 +1197,8 @@ const ChatList=({navigation}:any)=>{
                 hideModalContentWhileAnimating // Better performance, try with/without
                 propagateSwipe // Allows swipe events to propagate to children components (eg a ScrollView inside a modal)
                 style={styles.sideMenuStyle} // Needs to contain the width, 75% of screen width in our case
-            >       <MenuProvider style={{flex: 1, justifyContent: "flex-end"}}>
+            >
+                <MenuProvider style={{flex: 1, justifyContent: "flex-end"}}>
                 <Info otherParticipants={participants} close={toggleSideMenu}/>
             </MenuProvider>
 
