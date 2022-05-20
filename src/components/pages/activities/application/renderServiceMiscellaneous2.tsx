9da274b8aa69ec0f113d0581c8a6eeca068a3a78
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-05-20 11:18:26 +0800

    update

--- src/components/pages/activities/application/renderServiceMiscellaneous2.tsx
+++ src/components/pages/activities/application/renderServiceMiscellaneous2.tsx
@@ -98,7 +98,7 @@ const RenderServiceMiscellaneous=(props)=>{
                         break;
                     default:
                         let date = new RegExp(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/)
-                        result[p]= date.test(e) == true && Date.parse(e)>0 ? moment(e)?.format('LL') : (e?.replace("undefined", "") || e) ;
+                        result[p]= date.test(e) == true && Date.parse(e)>0 ? moment(e)?.format('LL') : (e?.replace(/undefined/g,'') || e) ;
                         break;
                 }
             })(obj);
