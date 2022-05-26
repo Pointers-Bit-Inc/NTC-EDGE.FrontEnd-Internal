da0f09f3cd58a3b53fe22300253f1fdf5b76e341
edwardlancelorilla <edwardlorilla1986@gmail.com>
2022-05-26 15:51:33 +0800

    update

--- src/components/pages/employee/index.tsx
+++ src/components/pages/employee/index.tsx
@@ -26,8 +26,70 @@ const style=StyleSheet.create({
         flex:1,
         margin:10,
     },
-    tableHeader:{fontSize:16,fontFamily:Regular500,fontWeight:"500",color:"#606A80"}
+    tableHeader:{fontSize:16,fontFamily:Regular500,fontWeight:"500",color:"#606A80"},
+    container:{backgroundColor:"#F8F8F8",flex:1,flexDirection:"row"},
+    title:{backgroundColor:"#fff",paddingHorizontal:46,paddingTop:22,paddingBottom:22,},
+    text:{color:"#113196",fontWeight:"600",fontSize:24,fontFamily:Bold},
+    search:{
+        paddingTop:20,
+        paddingBottom:7,
+        alignItems:"center",
+        justifyContent:"space-between",
+        flexDirection:"row",
+        flex:1
+    },
+    rightrow:{flexDirection:"row"},
+    filter:{
+        borderRadius:8,
+        paddingHorizontal:22,
+        paddingVertical:9,
+        backgroundColor:"#041B6E"
+    },
+    addParticipant:{
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        borderRadius:8,
+        paddingHorizontal:22,
+        paddingVertical:9,
+        backgroundColor:"#041B6E"
+    },
+    shadow:{
+        flex:1,
+        shadowColor:"rgba(0,0,0,0.1)",
+        shadowOffset:{width:0,height:4},
+        elevation:30,
+        shadowOpacity:1,
+        shadowRadius:10
+    },
+    headerTable:{
+        marginTop:35,
+        borderTopRightRadius:8,
+        borderTopLeftRadius:8,
+        marginHorizontal:45,
+        backgroundColor:"#fff",
+    },
+    headerTextContainer:{paddingVertical:16,paddingHorizontal:23},
+    flatlist:{
+        marginBottom:5,
+        borderBottomRightRadius:8,
+        borderBottomLeftRadius:8,
+        marginHorizontal:45,
+        flex:1,
+        backgroundColor:"#fff",
+    },
+    textTable:{fontFamily:Bold,color:"#606A80",fontSize:20,fontWeight:"600"},
+    contentContainer:{borderBottomColor:"#F0F0F0",borderBottomWidth:1},
+    pagination:{
+        paddingHorizontal:45,
+        paddingVertical:15,
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"flex-end"
+    }
 });
+
+
 export default function EmployeePage(props:any){
     const dimensions=useWindowDimensions();
     const [value,setValue]=useState('');
@@ -35,23 +97,81 @@ export default function EmployeePage(props:any){
     const [size,setSize]=useState(12);
     const [total,setTotal]=useState(10);
     const [docs,setDocs]=useState([]);
+    const [term,setTerm]=useState();
+    const [results,setResult]=useState([]);
 
+    useEffect(()=>{
+        const searchWiki=async()=>{
+            const {data}=await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    srsearch:term,
+                },
+            });
+
+            setDocs(data.query.search);
+        };
+        const timerId=setTimeout(()=>{
+            searchWiki();
+            // make a request after 1 second since there's no typing
+        },1000);
+
+        return ()=>{
+            clearTimeout(timerId);
+        };
+    },[term]);
     const user = useSelector((state: RootStateOrAny) => state.user);
     const config = {
         headers: {
             Authorization: "Bearer ".concat(user?.sessionToken)
         }
     }
-    const fetch = useCallback((_page?:number) => {
-        axios.get(BASE_URL + '/users', {...config, params: {
-                page: _page ? _page :page, pageSize: size,  ...(value&&{keyword:value}),
-            }}).then((response)=>{
-            setPage(response?.data?.page)
-            setSize(response?.data?.size)
-            setTotal(response?.data?.total)
-            setDocs(response?.data?.docs)
-        })
-    }, [page, size, total, value]);
+
+
+
+    const fetch = useCallback((_page?:number,text?:string)=>{
+        const search=async()=>{
+            const {data}=await axios.get(BASE_URL+'/users',{
+                ...config,params:{
+                    page:_page ? _page : page,pageSize:size,...(
+                        text&&{keyword:text}),
+                }
+            })
+            setPage(data?.page)
+            setSize(data?.size)
+            setTotal(data?.total)
+            setDocs(data?.docs)
+        };
+        const timerId = setTimeout(() => {
+            search();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, []);
+
+    useEffect(() => {
+        const search=async()=>{
+            const {data}=await axios.get(BASE_URL+'/users',{
+                ...config,params:{
+                    page:1,pageSize:size,...(
+                        value&&{keyword:value}),
+                }
+            })
+            setPage(data?.page)
+            setSize(data?.size)
+            setTotal(data?.total)
+            setDocs(data?.docs)
+        };
+        const timerId = setTimeout(() => {
+            search();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+
+    }, [value]);
      useEffect(()=>{
          fetch()
      }, [])
@@ -68,7 +188,11 @@ export default function EmployeePage(props:any){
                         searchWords={[value]}
                         textToHighlight={`${item.firstName} ${item.lastName}`}
                     /></Text>
-                    <Text style={[style.tableHeader,{fontSize:10,color:"#2863D6"}]}>{item.email}</Text>
+                    <Text style={[style.tableHeader,{fontSize:10,color:"#2863D6"}]}><Highlighter
+                        highlightStyle={{backgroundColor:'#BFD6FF'}}
+                        searchWords={[value]}
+                        textToHighlight={item.email}
+                    /></Text>
                 </View>
                 <View style={[style.cellStyle,{flex:0.5}]}>
                     <Text style={[style.tableHeader,{color:"#000000"}]}>{item?.role?.name}</Text>
@@ -86,10 +210,10 @@ export default function EmployeePage(props:any){
         var shownPages=3;
         var result=[];
         if(current>count-shownPages){
-            if(count-2 > 1){
+            if(count-2 >= 1){
                 result.push(count-2)
             }
-            if(count-1 > 1) {
+            if(count-1 >= 1) {
                 result.push(count-1)
             }
             if(count >= 1) {
@@ -107,58 +231,34 @@ export default function EmployeePage(props:any){
         return result;
     };
 
-    const textInputSearch = _.debounce( (text, ) => {
-        fetch()
 
-    }, 1000);
     return (
-        <View style={{backgroundColor:"#F8F8F8",flex:1,flexDirection:"row"}}>
+        <View style={style.container}>
             <View style={[styles.container,styles.shadow,{
                 flex:1,
             }]}>
-                <View style={{backgroundColor:"#fff",paddingHorizontal:46,paddingTop:22,paddingBottom:22,}}>
-                    <Text style={{color:"#113196",fontWeight:"600",fontSize:24,fontFamily:Bold}}>
+                <View style={style.title}>
+                    <Text style={style.text}>
                         Employees
                     </Text>
-                    <View style={{
-                        paddingTop:20,
-                        paddingBottom:7,
-                        alignItems:"center",
-                        justifyContent:"space-between",
-                        flexDirection:"row",
-                        flex:1
-                    }}>
+                    <View style={style.search}>
                         <View style={{flex:0.90,paddingRight:15}}>
                             <TextInput value={value} onChangeText={text=>{
                                 setValue(text)
-                                textInputSearch(text)
 
                             }} placeholderTextColor={"#6E7191"} placeholder={"Search"} style={styles.search}/>
                             <View style={styles.searchIcon}>
                                 <SearchIcon/>
                             </View>
                         </View>
-                        <View style={{flexDirection:"row"}}>
+                        <View style={style.rightrow}>
                             <View style={{paddingRight:10}}>
-                                <View style={{
-                                    borderRadius:8,
-                                    paddingHorizontal:22,
-                                    paddingVertical:9,
-                                    backgroundColor:"#041B6E"
-                                }}>
+                                <View style={style.filter}>
                                     <FilterOutlineIcon/>
                                 </View>
                             </View>
 
-                            <View style={{
-                                justifyContent:"center",
-                                alignItems:"center",
-                                flexDirection:"row",
-                                borderRadius:8,
-                                paddingHorizontal:22,
-                                paddingVertical:9,
-                                backgroundColor:"#041B6E"
-                            }}>
+                            <View style={style.addParticipant}>
                                 <View style={{paddingRight:10}}>
                                     <AddParticipantOutlineIcon color={"#fff"}/>
                                 </View>
@@ -170,26 +270,13 @@ export default function EmployeePage(props:any){
                     </View>
 
                 </View>
-                <View style={{
-                    flex:1,
-                    shadowColor:"rgba(0,0,0,0.1)",
-                    shadowOffset:{width:0,height:4},
-                    elevation:30,
-                    shadowOpacity:1,
-                    shadowRadius:10
-                }}>
-                    <View style={{
-                        marginTop:35,
-                        borderTopRightRadius:8,
-                        borderTopLeftRadius:8,
-                        marginHorizontal:45,
-                        backgroundColor:"#fff",
-                    }}>
+                <View style={style.shadow}>
+                    <View style={style.headerTable}>
 
-                        <View style={{paddingVertical:16,paddingHorizontal:23}}>
+                        <View style={style.headerTextContainer}>
 
                             <Text
-                                style={{fontFamily:Bold,color:"#606A80",fontSize:20,fontWeight:"600"}}>Employees</Text>
+                                style={style.textTable}>Employees</Text>
 
                         </View>
                     </View>
@@ -213,17 +300,10 @@ export default function EmployeePage(props:any){
                         </View>
                     </View>
 
-                    <View style={{
-                        marginBottom:5,
-                        borderBottomRightRadius:8,
-                        borderBottomLeftRadius:8,
-                        marginHorizontal:45,
-                        flex:1,
-                        backgroundColor:"#fff",
-                    }}>
+                    <View style={style.flatlist}>
 
                         <FlatList
-                            contentContainerStyle={{borderBottomColor:"#F0F0F0",borderBottomWidth:1}}
+                            contentContainerStyle={style.contentContainer}
                             data={docs}
                             keyExtractor={item=>item._id}
                             renderItem={renderItems}
@@ -231,13 +311,7 @@ export default function EmployeePage(props:any){
 
 
                     </View>
-                    <View style={{
-                        paddingHorizontal:45,
-                        paddingVertical:15,
-                        flexDirection:"row",
-                        alignItems:"center",
-                        justifyContent:"flex-end"
-                    }}>
+                    <View style={style.pagination}>
                         <TouchableOpacity onPress={()=> {
                             if(page-1 > 0){
                                 fetch(page-1 )
@@ -247,7 +321,7 @@ export default function EmployeePage(props:any){
                             <ChevronLeft/>
                         </TouchableOpacity>
 
-                        {pageNumbers(Math.floor((total/page)),page).map(number=>{
+                        {pageNumbers(Math.floor(total/page),page).map(number=>{
                             return <TouchableOpacity onPress={() => {
 
                                 fetch(number)
