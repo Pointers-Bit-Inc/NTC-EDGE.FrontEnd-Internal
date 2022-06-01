83a0c48cee025bc31aecb59b479c5b66a55d944c
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-01 08:06:26 +0800

    update

---
+++ src/components/atoms/left-side-web/index.tsx
@@ -0,0 +1,25 @@
+import {Platform, useWindowDimensions, View} from "react-native";
+import {styles} from "@pages/activities/styles";
+import {isMobile} from "@pages/activities/isMobile";
+import {isLandscapeSync, isTablet} from "react-native-device-info";
+import React from "react";
+
+function LeftSideWeb(props) {
+    const dimensions=useWindowDimensions();
+    return <View style={[styles.container, styles.shadow, {
+        flexBasis: (
+            (
+                isMobile && !(
+                    Platform?.isPad || isTablet())) || dimensions?.width < 768 || (
+                (
+                    Platform?.isPad || isTablet()) && !isLandscapeSync())) ? "100%" : 466,
+        flexGrow: 0,
+        flexShrink: 0,
+        backgroundColor: "#FFFFFF"
+    }]}>
+        {props.children}
+    </View>;
+}
+
+
+export default LeftSideWeb
