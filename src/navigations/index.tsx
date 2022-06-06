7283e3d9a74f9c51aa20780b699dbd8772d54465
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-06 10:55:16 +0800

    revert

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {NavigationContainer, StackActions, useNavigation} from '@react-navigation/native';
+import {useEffect,useState} from 'react';
+import {NavigationContainer,StackActions,useNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import lodash from 'lodash';
 import ForgotPassword from './forgot-password';
@@ -21,31 +21,27 @@ import MeetingParticipants from '@screens/meet/participants';
 import NewChat from '@screens/chat/new-chat';
 import Search from "@pages/activities/search";
 import TabBar from "@pages/activities/tabbar";
-import {Platform, View} from "react-native";
+import {Platform,Text,TouchableOpacity,View} from "react-native";
 import {primaryColor} from "@styles/color";
 import EdgeLogo from "@assets/svg/edge";
 import SettingTopBar from "@assets/svg/settingTopBar";
 import HelpTopBar from "@assets/svg/helpTopbar";
-import {RootStateOrAny, useDispatch, useSelector} from "react-redux";
+import {RootStateOrAny,useDispatch,useSelector} from "react-redux";
+import ProfileImage from "@atoms/image/profile";
 import {isMobile} from "@pages/activities/isMobile";
 import Login from "@screens/login/login";
 import FloatingVideo from '@components/pages/chat-modal/floating-video';
 import {useComponentLayout} from "../hooks/useComponentLayout";
-import {
-    setApplicationItem,
-    setApplications,
-    setNotPinnedApplication,
-    setPinnedApplication,
-    setTopBarNav
-} from "../reducers/application/actions";
+import {setTopBarNav} from "../reducers/application/actions";
+import axios from "axios";
+import {BASE_URL} from "../services/config";
+import {ToastType} from "@atoms/toast/ToastProvider";
+import {Menu,MenuOption,MenuOptions,MenuTrigger} from "react-native-popup-menu";
+import DotHorizontalIcon from "@assets/svg/dotHorizontal";
 import ProfileMenu from "@molecules/headerRightMenu";
+import useLogout from "../hooks/useLogout";
+import Alert from "@atoms/alert";
 import CustomAlert from "@pages/activities/alert/alert";
-import {setResetFilterStatus} from "../reducers/activity/actions";
-import {resetUser} from "../reducers/user/actions";
-import {resetMeeting} from "../reducers/meeting/actions";
-import {resetChannel} from "../reducers/channel/actions";
-import useOneSignal from "../hooks/useOneSignal";
-import Api from "../services/api";
 
 type RootStackParamList = {
     App: undefined;
@@ -99,25 +95,15 @@ const linking = {
             Dashboard: 'Dashboard',
         }
     },
-  };
+};
 
 
 const RootNavigator = () => {
     const user = useSelector((state: RootStateOrAny) => state.user) || {};
-    const dispatch = useDispatch();
+    const dispatch=useDispatch();
     const [visible, setVisible] = useState(false);
-    const {meeting} = useSelector((state: RootStateOrAny) => state.meeting);
-    const onLogout = () => {
-        dispatch(setApplications([]))
-        dispatch(setPinnedApplication([]))
-        dispatch(setNotPinnedApplication([]))
-        dispatch(setApplicationItem({}))
-        dispatch(setResetFilterStatus([]))
-        dispatch(resetUser());
-        dispatch(resetMeeting());
-        dispatch(resetChannel());
-    }
-
+    const { meeting } = useSelector((state:RootStateOrAny) => state.meeting);
+    const onLogout = useLogout(user, dispatch)
     const renderFloatingVideo = () => {
         if (Platform?.OS === 'web') {
             return null
@@ -135,45 +121,44 @@ const RootNavigator = () => {
 
             <Stack.Navigator
 
-                screenOptions={{
-                    gestureEnabled: false,
-                    headerShown: false,
+                screenOptions={ {
+                    gestureEnabled : false ,
+                    headerShown : false ,
 
-                }}
+                } }
 
                 initialRouteName="App"
             >
 
-                <Stack.Screen name="App" component={App}/>
-                <Stack.Screen name="AppIntro" component={AppIntro}/>
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
-                <Stack.Screen name="ActivitiesScreen" component={TabBar} options={
+                <Stack.Screen name="App" component={ App }/>
+                <Stack.Screen name="AppIntro" component={ AppIntro }/>
+                <Stack.Screen name="Login" component={ Login }/>
+                <Stack.Screen name="ForgotPassword" component={ ForgotPassword }/>
+                <Stack.Screen name="ActivitiesScreen" component={ TabBar } options={
                     {
-                        title: null,
-                        headerRight: () => {
-                            const dispatch = useDispatch();
-                            const [activitySizeComponent, onActivityLayoutComponent] = useComponentLayout();
-                            useEffect(() => {
+                        title : null ,
+                        headerRight : () => {
+                            const dispatch=useDispatch();
+                            const [activitySizeComponent,onActivityLayoutComponent]=useComponentLayout();
+                            useEffect(()=>{
                                 dispatch(setTopBarNav(activitySizeComponent))
                             }, [activitySizeComponent?.width])
                             const navigation = useNavigation();
-                            const {destroy} = useOneSignal(user);
-                            return <View onLayout={onActivityLayoutComponent} style={{flexDirection: "row"}}>
-                                <View style={{paddingRight: 32}}>
+                            return <View onLayout={onActivityLayoutComponent} style={{flexDirection:"row"}}>
+                                <View style={{paddingRight:32}}>
                                     <SettingTopBar height={26} width={26}></SettingTopBar>
                                 </View>
-                                <View style={{paddingRight: 32}}>
+                                <View style={{paddingRight:32}}>
                                     <HelpTopBar height={26} width={26}></HelpTopBar>
                                 </View>
-                                <View style={{paddingRight: 32}}>
+                                <View style={{paddingRight:32}}>
 
-                                    <ProfileMenu onClose={() => {
+                                    <ProfileMenu onClose={()=>{
 
-                                    }} onSelect={value => {
-                                        if (value == "editProfile") {
+                                    }} onSelect={value=>{
+                                        if(value=="editProfile"){
                                             navigation.navigate('UserProfileScreen')
-                                        } else if (value == "logout") {
+                                        } else if(value=="logout"){
                                             setVisible(true)
                                         }
                                     }} user={user}/>
@@ -186,12 +171,10 @@ const RootNavigator = () => {
                                         cancelText='Cancel'
                                         onConfirmPressed={() => {
                                             setVisible(false)
-                                            const api=Api(user.sessionToken);
                                             onLogout()
-                                            destroy();
                                             navigation.dispatch(StackActions.replace('Login'));
                                         }}
-                                        onDismissed={() => {
+                                        onDismissed={()=>{
                                             setVisible(false)
                                         }}
                                         onCancelPressed={() => {
@@ -204,15 +187,15 @@ const RootNavigator = () => {
 
                             </View>
                         },
-                        headerLeft: () => (
-                            <View style={{paddingLeft: 32}}>
-                                <EdgeLogo width={127} height={29}/>
+                        headerLeft : () => (
+                            <View style={ { paddingLeft : 32 } }>
+                                <EdgeLogo width={ 127 } height={ 29 }/>
                             </View>
-                        ),
-                        headerShown: (isMobile && !Platform?.isPad) ? false : true,
-                        headerStyle: {
+                        ) ,
+                        headerShown : (isMobile && !Platform?.isPad) ? false : true ,
+                        headerStyle : {
 
-                            backgroundColor: primaryColor,
+                            backgroundColor : primaryColor ,
                         }
                     }
                 }/>
@@ -230,8 +213,7 @@ const RootNavigator = () => {
                 <Stack.Screen name="SearchActivities" component={ Search }/>
             </Stack.Navigator>
             {renderFloatingVideo()}
-            {visible && <View
-                style={{position: "absolute", width: "100%", height: "100%", backgroundColor: "rgba(0,0,0,0.5)"}}/>}
+            {visible && <View style={{position: "absolute", width: "100%", height: "100%",backgroundColor: "rgba(0,0,0,0.5)"}}/>}
         </NavigationContainer>
 
     );
