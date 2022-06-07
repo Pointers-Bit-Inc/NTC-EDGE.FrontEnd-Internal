015d5ac9388b4620209d253f67410b45f657c201
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-07 14:37:05 +0800

    update

--- src/reducers/application/reducers.ts
+++ src/reducers/application/reducers.ts
@@ -115,7 +115,7 @@ export default function basket(state = initialState, action = {}) {
             for (let i = 0; i < action.payload?.data?.length; i++) {
 
                 if ((
-                    (action.payload?.data?.[i]?.assignedPersonnel?._id || action.payload?.data?.[i]?.assignedPersonnel ) == action.payload?.user?._id) && (cashier ?  action.payload?.data?.[i]?.paymentHistory?.length : true )
+                    (action.payload?.data?.[i]?.assignedPersonnel?._id || action.payload?.data?.[i]?.assignedPersonnel ) == action.payload?.user?._id)
                     ) {
 
                     isPinned.push(action.payload?.data?.[i])
