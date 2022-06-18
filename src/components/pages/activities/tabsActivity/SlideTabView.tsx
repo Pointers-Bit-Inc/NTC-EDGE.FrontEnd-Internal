2875fe867d7af3aead5b1461b215934d4e7f1613
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-18 15:58:10 +0800

    revert

--- src/components/pages/activities/tabsActivity/SlideTabView.tsx
+++
@@ -1,3 +0,0 @@
-import createHeaderTabsComponent  from './createHeaderTabsComponent'
-import ScrollableTabView from "@pages/activities/tabs";
-export default createHeaderTabsComponent(ScrollableTabView, { slideAnimated: true })
