41bd4a3bff915af23a46ccb3355f693209f52f16
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-22 03:12:50 +0800

    update

--- src/components/pages/activities/index.tsx
+++ src/components/pages/activities/index.tsx
@@ -579,13 +579,13 @@ export default function ActivitiesPage(props: any) {
                             act?.assignedPersonnel?._id||act?.assignedPersonnel)==user?._id)).length)}
                         ListHeaderComponent={header ? listHeaderComponent() : null}
                         contentContainerStyle={{
-                           // paddingTop: HeaderHeight + TabBarHeight + (!!lodash.size(meetingList) && 80),
+                           paddingTop: Platform.OS != "ios" ?HeaderHeight + TabBarHeight + (!!lodash.size(meetingList) && 80) : 0,
                         }}
                         contentInset={{top: HeaderHeight + TabBarHeight + (!!lodash.size(meetingList) && 80)}}
                        contentOffset={{y: -HeaderHeight + TabBarHeight + (!!lodash.size(meetingList) && 80)}}
                         refreshControl={
                             <RefreshControl
-
+                                progressViewOffset={HeaderHeight + TabBarHeight + (!!lodash.size(meetingList) && 80)}
                                 refreshing={refreshing}
                                 onRefresh={onRefresh}
                             />
