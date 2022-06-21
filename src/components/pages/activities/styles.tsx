2b53ff2b83ad5f28a6722b55e0339f96a3a1eb8d
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-21 17:58:50 +0800

    revert

--- src/components/pages/activities/styles.tsx
+++ src/components/pages/activities/styles.tsx
@@ -44,7 +44,7 @@ export const styles = StyleSheet.create({
     rect: {
         ...Platform.select({
             native: {
-                paddingTop : 0,
+                paddingTop : 40 ,
                 padding : 30 ,
             },
             default: {
