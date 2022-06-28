9063f05fc1f73b397f582c1cea95e0fc5a9113f0
Edward Lance lorilla <edwardlorilla1986@gmail.com>
2022-06-28 15:45:20 +0800

    update

--- src/components/pages/activities/index.tsx
+++ src/components/pages/activities/index.tsx
@@ -780,7 +780,7 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         backgroundColor: "white",
-        justifyContent: "center",
+        justifyContent: "flex-end",
         zIndex: 1,
     },
     headerContainer: {
