4a8ccc86ca3df0a7c212443e5901255462ad8662
Edward Lance Lorilla <edwardlance.lorilla@ustp.edu.ph>
2024-09-17 12:58:27 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+const aliases = {
+  "@assets": "./assets",
+  "@styles": "./src/styles",
+  "@components": "./src/components",
+  "@atoms": "./src/components/atoms",
+  "@molecules": "./src/components/molecules",
+  "@organisms": "./src/components/organisms",
+  "@templates": "./src/components/templates",
+  "@pages": "./src/components/pages",
+  "@screens": "./src/screens",
+  "@reducers": "./src/reducers",
+  "@utils": "./src/utils"
+};
+
+const babelLoaderRules = [{
+  test: /\.ts/,
+  loader: 'babel-loader',
+  options: {
+    presets: ['babel-preset-expo'],
+  },
+}];
+
+// Expo CLI will await this method so you can optionally return a promise.
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // If you want to add a new alias to the config.
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    ...aliases
+  };
+  config.resolve.extensions.push('.web.js', '.web.ts', '.web.tsx');
+  config.module.rules = [
+    ...config.module.rules,
+    ...babelLoaderRules,
+  ];
+  config.devtool = false;
+  config.optimization = {
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000,
+    }
+  };
+  config.performance = {
+    hints: false
+  }
+  config.mode = 'production';
+  // Finally return the new config for the CLI to use.
+  return config;
+};
