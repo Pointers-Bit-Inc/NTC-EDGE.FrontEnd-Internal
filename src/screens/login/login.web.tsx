bd33f1432af2f9427e2046f94442a51508362540
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-09-19 01:47:24 +0800

    update

--- src/screens/login/login.web.tsx
+++ src/screens/login/login.web.tsx
@@ -3,7 +3,7 @@ import {
   Easing,
   ImageBackground,
   Modal,
-  Animated,
+  ScrollView,
   StatusBar,
   TouchableOpacity,
   TouchableWithoutFeedback,
@@ -21,13 +21,12 @@ import { Regular500 } from "@styles/font";
 import ForgotPassword from "./../../navigations/forgot-password";
 import CloseIcon from "@assets/svg/close";
 
-import { useSharedValue, useAnimatedStyle, withTiming, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, withSpring } from 'react-native-reanimated';
 import { isMobile } from "@/src/utils/formatting";
 import { useAuth } from '@/src/hooks/useAuth';
 import Footer from '@atoms/footer';
 
 const background = require("@assets/webbackground.png");
-
 
 const Login = ({ navigation }: any) => {
     const { width, height } = useWindowDimensions();
@@ -59,106 +58,113 @@ const Login = ({ navigation }: any) => {
             transform: [{ translateY: formTranslateY.value }],
         };
     });
+  const translateY = useSharedValue(height); // Start with the footer off-screen
 
-
+  useEffect(() => {
+    translateY.value = withSpring(0, {
+      damping: 100,
+      stiffness: 100,
+      mass: 1,
+    });
+  }, []);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  });
   return (
+      <View style={styles.container}>
           <ImageBackground
             resizeMode="cover"
             source={background}
             style={{
-              flex: 1,
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
+                flex: 1,
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
             }}
             imageStyle={{ flex: 1 }}
           >
-            <StatusBar barStyle="dark-content" />
+              <StatusBar barStyle="dark-content" />
 
-            <View style={{ transform: [{ scale: 0.90 }], flex: 1, justifyContent: "center", alignItems: "center" }}>
-              {/* Animated logo */}
-              <Animated.View style={[{ paddingBottom: 40 }, logoAnimatedStyle]}>
-                <EdgeBlue width={342} height={78} />
-              </Animated.View>
+              <View style={{ transform: [{ scale: 0.90 }], flex: 1, justifyContent: "center", alignItems: "center" }}>
+                  {/* Animated logo */}
+                  <Animated.View style={[{ paddingBottom: 40 }, logoAnimatedStyle]}>
+                      <EdgeBlue width={342} height={78} />
+                  </Animated.View>
 
-              {/* Animated form */}
-              <Animated.View style={[styles.formContainer, formAnimatedStyle]}>
-                <Text style={[styles.formTitleText]}>Login</Text>
+                  {/* Animated form */}
+                  <Animated.View style={[styles.formContainer, formAnimatedStyle]}>
+                      <Text style={[styles.formTitleText]}>Login</Text>
 
-                <LoginForm onChangeValue={onChangeValue} form={formValue} />
+                      <LoginForm onChangeValue={onChangeValue} form={formValue} />
 
-                <View style={styles.bottomContainer}>
-                  <Button
-                    testID={"login-button"}
-                    style={[
-                      styles.loginButton,
-                      {
-                        backgroundColor: loading ? button.info : isValid ? button.primary : button.default,
-                      },
-                    ]}
-                    disabled={loading}
-                    onPress={onCheckValidation}
-                  >
-                    {loading ? (
-                      <View style={{ paddingVertical: 10 }}>
-                        <Ellipsis color="#fff" size={10} />
+                      <View style={styles.bottomContainer}>
+                          <Button
+                            testID={"login-button"}
+                            style={[
+                                styles.loginButton,
+                                {
+                                    backgroundColor: loading ? button.info : isValid ? button.primary : button.default,
+                                },
+                            ]}
+                            disabled={loading}
+                            onPress={onCheckValidation}
+                          >
+                              {loading ? (
+                                <View style={{ paddingVertical: 10 }}>
+                                    <Ellipsis color="#fff" size={10} />
+                                </View>
+                              ) : (
+                                <View>
+                                    <Text style={styles.boldText} color={isValid ? "#fff" : text.disabled} size={18}>
+                                        Login
+                                    </Text>
+                                </View>
+                              )}
+                          </Button>
                       </View>
-                    ) : (
-                      <View>
-                        <Text style={styles.boldText} color={isValid ? "#fff" : text.disabled} size={18}>
-                          Login
-                        </Text>
-                      </View>
-                    )}
-                  </Button>
-                </View>
 
-                {!isMobile && (
-                  <View style={[{ paddingTop: 15, justifyContent: "flex-start" }]}>
-                    <TouchableOpacity onPress={() => setForgotPasswordModal(true)}>
-                      <Text style={[{ fontSize: 18, fontFamily: Regular500, color: text.info }]}>Forgot your
-                        password?</Text>
-                    </TouchableOpacity>
-                  </View>
-                )}
-              </Animated.View>
-            </View>
-
-
-            <Footer/>
-            <Modal transparent={true} visible={forgotPasswordModal}>
-              <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                <TouchableWithoutFeedback onPress={() => setForgotPasswordModal(false)}>
-                  <View
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      alignItems: "center",
-                      justifyContent: "flex-end",
-                      position: "absolute",
-                      backgroundColor: "rgba(0,0,0, 0.5)",
-                    }}
-                  />
-                </TouchableWithoutFeedback>
-                <View style={{
-                  padding: 20,
-                  backgroundColor: "#fff",
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  width: width * 0.28,
-                  flex: 0.77
-                }}>
-                  <TouchableOpacity onPress={() => setForgotPasswordModal(false)}>
-                    <View style={{ paddingRight: 20, alignItems: "flex-end" }}>
-                      <CloseIcon />
-                    </View>
-                  </TouchableOpacity>
-
-                  <ForgotPassword />
-                </View>
+                      {!isMobile && (
+                        <View style={[{ paddingTop: 15, justifyContent: "flex-start" }]}>
+                            <TouchableOpacity onPress={() => setForgotPasswordModal(true)}>
+                                <Text style={[{ fontSize: 18, fontFamily: Regular500, color: text.info }]}>Forgot your password?</Text>
+                            </TouchableOpacity>
+                        </View>
+                      )}
+                  </Animated.View>
               </View>
-            </Modal>
+
+              <Footer></Footer>
+
+              {/* Forgot password modal */}
+              <Modal transparent={true} visible={forgotPasswordModal}>
+                  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                      <TouchableWithoutFeedback onPress={() => setForgotPasswordModal(false)}>
+                          <View
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                alignItems: "center",
+                                justifyContent: "flex-end",
+                                position: "absolute",
+                                backgroundColor: "rgba(0,0,0, 0.5)",
+                            }}
+                          />
+                      </TouchableWithoutFeedback>
+                      <View style={{ padding: 20, backgroundColor: "#fff", borderColor: "#E5E5E5", borderRadius: 10, width: width * 0.28, flex: 0.77 }}>
+                          <TouchableOpacity onPress={() => setForgotPasswordModal(false)}>
+                              <View style={{ paddingRight: 20, alignItems: "flex-end" }}>
+                                  <CloseIcon />
+                              </View>
+                          </TouchableOpacity>
+
+                          <ForgotPassword />
+                      </View>
+                  </View>
+              </Modal>
           </ImageBackground>
+      </View>
     );
 };
 
